60dcafea5cdad2677918c2953606799bba8b346c
omelniz <omelniz@mail.ru>
2015-06-27 16:29:26 +0600

    Fix readme and add auto init

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Limiter for textarea
 
 Just insert text-limit.js
-<script src="js/text-limit.js"></script>
+
+	<script src="js/text-limit.js"></script>
 
 and use
-<textarea data-limit="256" data-label="textarea-limit"></textarea>
 
-textarea-limit is id for label
+	<textarea data-limit="256" data-label="textarea-limit"></textarea>
 
-<p id="textarea-limit" class="form-limit"></p>
+	data-limit="256" is limit of symbols
+	data-label="textarea-limit" is id for label
+
+	Example label:
+
+	<p id="textarea-limit" class="form-limit"></p>
 
--- text-limit.js
+++ text-limit.js
@@ -1,74 +1,81 @@
-function declOfNum(number, titles) {
-    cases = [2, 0, 1, 1, 1, 2];
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
-}
+(function () {
+    "use strict";
 
-$.fn.limiter = function(options) {
-
-    var options = $.extend({}, options);
-
-    var Limiter = function (options) {
-        var $field = $(options.el),
-            $limitLabel = $("#" + $field.data("label")),
-            limit = parseInt($field.data("limit"), 10) || false;
-
-        function init () {
-            if (limit) {
-                bindEvents();
-                onChangeField();
-            }
-        }
-
-        function bindEvents () {
-            $field.on("keyup keypress input cut copy paste change click", onChangeField);
-        }
-
-        function onChangeField () {
-            var wordcount = getWordcount();
-
-            if (isLimit(wordcount)) {
-                $field.val($field.val().substr(0, limit));
-                wordcount = getWordcount();
-            }
-
-            var count = limit - wordcount,
-                label = [
-                    // declOfNum(count, ['Остался', 'Осталось', 'Осталось']),
-                    count,
-                    declOfNum(count, ['символ', 'символа', 'символов'])
-                ].join(" ");
-
-            $limitLabel.text(label);
-        }
-
-        function getWordcount () {
-            return $field.val().length;
-        }
-
-        function isLimit (wordcount) {
-            if (wordcount >= limit) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        function isShowLabel (wordcount) {
-            if (wordcount >= countOfShowLimit) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        init();
-    };
-
-    function make () {
-        new Limiter({el: this});
+    function declOfNum(number, titles) {
+        cases = [2, 0, 1, 1, 1, 2];
+        return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
     }
 
-    return this.each(make);
-};
+    $.fn.limiter = function(options) {
+
+        var options = $.extend({}, options);
+
+        var Limiter = function (options) {
+            var $field = $(options.el),
+                $limitLabel = $("#" + $field.data("label")),
+                limit = parseInt($field.data("limit"), 10) || false;
+
+            function init () {
+                if (limit) {
+                    bindEvents();
+                    onChangeField();
+                }
+            }
+
+            function bindEvents () {
+                $field.on("keyup keypress input cut copy paste change click", onChangeField);
+            }
+
+            function onChangeField () {
+                var wordcount = getWordcount();
+
+                if (isLimit(wordcount)) {
+                    $field.val($field.val().substr(0, limit));
+                    wordcount = getWordcount();
+                }
+
+                var count = limit - wordcount,
+                    label = [
+                        declOfNum(count, ['Остался', 'Осталось', 'Осталось']),
+                        count,
+                        declOfNum(count, ['символ', 'символа', 'символов'])
+                    ].join(" ");
+
+                $limitLabel.text(label);
+            }
+
+            function getWordcount () {
+                return $field.val().length;
+            }
+
+            function isLimit (wordcount) {
+                if (wordcount >= limit) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            function isShowLabel (wordcount) {
+                if (wordcount >= countOfShowLimit) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            init();
+        };
+
+        function make () {
+            new Limiter({el: this});
+        }
+
+        return this.each(make);
+    };
+
+    // Auto initialization
+    $("[data-limit]").limiter();
+})();
